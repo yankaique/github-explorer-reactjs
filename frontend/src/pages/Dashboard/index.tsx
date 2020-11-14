a896ec198a9a1c3d0b97d0d684bb45d9c16abb9a
Yan <yankaiquecosta.yk@gmail.com>
2020-11-14 16:14:24 -0300

    Styling dashboard page

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo-github-explorer.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49956758?s=460&u=11861943fb68ef85c0900067bad0db69a11c7869&v=4"
+            alt="Yan"
+          />
+          <div>
+            <strong>o-matador-de-monstros</strong>
+            <p>Game developed in Vue JS</p>
+          </div>
+          <FiChevronRight size={16} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49956758?s=460&u=11861943fb68ef85c0900067bad0db69a11c7869&v=4"
+            alt="Yan"
+          />
+          <div>
+            <strong>o-matador-de-monstros</strong>
+            <p>Game developed in Vue JS</p>
+          </div>
+          <FiChevronRight size={16} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49956758?s=460&u=11861943fb68ef85c0900067bad0db69a11c7869&v=4"
+            alt="Yan"
+          />
+          <div>
+            <strong>o-matador-de-monstros</strong>
+            <p>Game developed in Vue JS</p>
+          </div>
+          <FiChevronRight size={16} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
